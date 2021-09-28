1abef153e9da3b6154709934e0b29b23de8f5dad
Gebremeskel <gebre20122@gmail.com>
2021-09-28 12:18:01 -0500

    writng const variable for port, document, form

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('hello!!')
+const port = `http://localhost:3000`;
+const tripCall = new TripService(port);
+const locationCall = new LocationService(port);
+const form = document.getElementById("trip-form")
+const dropDown = document.getElementById("location-dropdown")
+const nameValue = document.getElementById("trip-name")
+const addressValue = document.getElementById("trip-address")
+const budgetValue = document.getElementById("trip-budget")
+
+tripCall.getTrips()
+locationCall.getLocations()
+
+form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(e) {
+    e.preventDefault();
+    tripCall.createTrips()
+    // debugger
+}
