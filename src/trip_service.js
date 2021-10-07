88a3a65c8ede86b983255448ce702c132830137d
Gebremeskel <gebre20122@gmail.com>
2021-10-07 10:38:10 -0500

    assign t, loc, x to variable & adding if (!loc)

--- src/trip_service.js
+++ src/trip_service.js
@@ -3,28 +3,34 @@ class TripService {
         this.port = port
     }
 
+    // render trips 
     getTrips() {
         fetch(this.port + `/trips`)
         .then(response => response.json())
-        .then(data => {
-            for(const trip of data) {
-               let t = new Trip(trip)
+        .then(json => {
+           //debugger
+            for(const trip of json.data) {
+               let t = new Trip({id: trip.id, ...trip.attributes})
+              // debugger
                t.attachToDom()
             }
         })
-        .catch()
     }
+    
 
     createTrips() {
+        //debugger
         const tripInfo = {
-            //debugger
-            trip: {
+            
+            trip: {            
                 name: nameValue.value,
                 address: addressValue.value,
                 budget: budgetValue.value,
-                location_id: dropDown.value
-            }
+                //location_id: dropdown.value
+                location_name: locationValue.value               
+            }          
         }
+        //debugger
         const configObject = {
             method: 'POST',
             headers: {
@@ -37,9 +43,26 @@ class TripService {
         //debugger
        fetch(this.port + `/trips`, configObject)
        .then(response => response.json())
-       .then(data => {
-        const t = new Trip(data)
+       .then(json => {
+        //debugger
+        const t = new Trip(json)
+        //debugger  
+        const loc = Location.all.find(l => parseInt(l.id) === t.locationId)
+        //debugger
+        if(!loc) {
+            //debugger
+            const x = new Location({
+                id: json.data.attributes.location_id, 
+                name: json.data.attributes.location_name        
+                // debugger
+            })
+            //debugger
+                x.addToDom()
+                x.addToDropDown()
+        }
+        //debugger
         t.attachToDom()
+
        })
     }
 
