7fd1fe49c8fbd984ed7849e78e945d48b1a2bce6
Gebremeskel <gebre20122@gmail.com>
2021-10-07 10:28:45 -0500

    assign ul & locationValue variable, e.target.reset

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const port = `http://localhost:3000`;
 const tripCall = new TripService(port);
 const locationCall = new LocationService(port);
+const dropdown = document.getElementById("location-dropdown")
+const ul = document.getElementById('trip-list');
 const form = document.getElementById("trip-form")
-const dropDown = document.getElementById("location-dropdown")
 const nameValue = document.getElementById("trip-name")
 const addressValue = document.getElementById("trip-address")
 const budgetValue = document.getElementById("trip-budget")
+const locationValue = document.getElementById("location-id")
 
 tripCall.getTrips()
 locationCall.getLocations()
@@ -16,4 +18,5 @@ function handleSubmit(e) {
     e.preventDefault();
     tripCall.createTrips()
     // debugger
+    e.target.reset();  //after creating to clear the data
 }
