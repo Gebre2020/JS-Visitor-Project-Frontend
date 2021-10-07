103e9e1a864826cac7ca5de03aaa80980210dfbc
Gebremeskel <gebre20122@gmail.com>
2021-10-07 10:41:01 -0500

    adding iterate for location

--- src/location_service.js
+++ src/location_service.js
@@ -7,15 +7,19 @@ class LocationService {
         fetch(`${this.port}/locations`)
         .then(response => response.json())
         .then(json => {
-            json.forEach(element => {
-                //debugger
+            //debugger
+            for(const location of json.data) {
+                let l = new Location({id: location.id, ...location.attributes})
+           // json.forEach(element => {
+            //json["data"].forEach(element => {
+              //debugger
                 //dropDown.value
-                const l = new Location(element)
+                //const l = new Location(element)
                 //debugger
+                // const l = new Location({id: element.id, ...element.attributes})
+                l.addToDom()
                 l.addToDropDown()
-
-            })
-          
+            }        
         })
     }
 
